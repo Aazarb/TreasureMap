c29fa21125f4a51dc528788d5c746e914302ed7d
Aazarb <zaraba.adil@hotmail.fr>
2025-04-18 12:50:56 +0200

    Ajout du service de d'ecriture en sortie de simulation de carte - methode drawMap() pour le dessin de la carte (avant et après simulation) - methode writingMap() pour encoder la carte après simulation

--- src/main/java/com/carbonit/treasuremap/services/OutputMapService.java
+++ src/main/java/com/carbonit/treasuremap/services/OutputMapService.java
@@ -1,7 +1,114 @@
 package com.carbonit.treasuremap.services;
 
+import com.carbonit.treasuremap.enums.BoxTypeEnum;
+import com.carbonit.treasuremap.models.Adventurer;
+import com.carbonit.treasuremap.models.Box;
+import com.carbonit.treasuremap.models.TreasureMap;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class OutputMapService {
+
+    public String drawMap(TreasureMap treasureMap) {
+        Box[][] grid = treasureMap.getGrid();
+        int rows = grid.length;     // Nombre de lignes (axe vertical)
+        int columns = grid[0].length; // Nombre de colonnes (axe horizontal)
+
+        // Initialisation de la carte en considérant ses cases comme des PLAINES
+        String[][] drawnMap = drawPlainMap(columns, rows);
+
+        // Remplir les montagnes et trésors en transposant les coordonnées
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                Box currentCase = grid[i][j];
+
+                if (currentCase.isMountainBox()) {
+                    drawnMap[j][i] = "M";  // Transposition: [j][i] au lieu de [i][j]
+                } else if (currentCase.isTreasureBox()) {
+                    drawnMap[j][i] = "T(" + currentCase.getTreasureNb() + ")";  // Transposition
+                }
+            }
+        }
+        // Ajouter les aventuriers avec transposition
+        if(treasureMap.getAdventurer() != null) {
+            Adventurer adventurer = treasureMap.getAdventurer();
+
+                int adventurerRow = adventurer.getX();
+                int adventurerColumn = adventurer.getY();
+
+                // Transposition des coordonnées
+                drawnMap[adventurerColumn][adventurerRow] = "A(" + adventurer.getName() + ")";
+        }
+
+        return getStringDrawnMap(columns, rows, drawnMap);
+    }
+
+    private static String getStringDrawnMap(int columns, int rows, String[][] drawnMap) {
+
+        // Construction de la string finale
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < columns; i++) {  // Parcourir d'abord les colonnes (qui deviennent lignes)
+            for (int j = 0; j < rows; j++) {  // Puis les lignes (qui deviennent colonnes)
+                stringBuilder.append(drawnMap[i][j]);
+                if (j < rows - 1) {
+                    stringBuilder.append(" ");
+                }
+            }
+            if (i < columns - 1) {
+                stringBuilder.append("\n");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private static String[][] drawPlainMap(int columns, int rows) {
+
+        // Créer une matrice transposée pour l'affichage
+
+        String[][] plainMap = new String[columns][rows];  // Inverser dimensions pour la transposition
+
+        // Dessiner toutes les cases comme des plaines
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < rows; j++) {
+                plainMap[i][j] = ".";
+            }
+        }
+        return plainMap;
+    }
+
+    public String writingMap(TreasureMap treasureMap) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n");
+
+        List<Box> listTreasureBoxes = Arrays.stream(treasureMap.getGrid())
+                .flatMap(Arrays::stream)                             // Aplati chaque ligne en un flux de Box (Box[])
+                .filter(boxItem -> boxItem.getBoxType() == BoxTypeEnum.TREASURE)
+                .toList();
+
+
+        List<Box> listMountainBoxes = Arrays.stream(treasureMap.getGrid())
+                .flatMap(Arrays::stream)
+                .filter(boxItem -> boxItem.getBoxType() == BoxTypeEnum.MOUNTAIN)
+                .toList();
+
+        stringBuilder.append("C - ").append(treasureMap.getWidth()).append(" - ").append(treasureMap.getHeight()).append("\n");
+        for( Box mountainBox : listMountainBoxes){
+            stringBuilder.append("M - ").append(mountainBox.getX()).append(" - ").append(mountainBox.getY()).append("\n");
+        }
+
+        for( Box tresor : listTreasureBoxes){
+            stringBuilder.append("T - ").append(tresor.getX()).append(" - ").append(tresor.getY()).append(" - ").append(tresor.getTreasureNb()).append("\n");
+        }
+        if(treasureMap.getAdventurer() != null) {
+            Adventurer adventurer = treasureMap.getAdventurer();
+            stringBuilder.append("A - ").append(adventurer.getName()).append(" - ").append(adventurer.getX())
+                    .append(" - ").append(adventurer.getY()).append(" - ").append(adventurer.getDirection().toString())
+                    .append(" - ").append(adventurer.getTreasureFound()).append("\n");
+        }
+
+        return stringBuilder.toString().trim();
+    }
 }
--- src/test/java/com/carbonit/treasuremap/services/OutputMapServiceTest.java
+++ src/test/java/com/carbonit/treasuremap/services/OutputMapServiceTest.java
@@ -1,7 +1,155 @@
 package com.carbonit.treasuremap.services;
 
+import com.carbonit.treasuremap.enums.BoxTypeEnum;
+import com.carbonit.treasuremap.enums.DirectionEnum;
+import com.carbonit.treasuremap.models.Adventurer;
+import com.carbonit.treasuremap.models.Box;
+import com.carbonit.treasuremap.models.TreasureMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class OutputMapServiceTest {
+    @Autowired
+    private OutputMapService outputService;
+    private TreasureMap treasureMap;
 
+    @BeforeEach
+    void setUp() {
+        this.treasureMap = new TreasureMap();
+        Box[][] grid = new Box[5][5];
+        this.treasureMap.setWidth(5);
+        this.treasureMap.setHeight(5);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                grid[i][j] = new Box(i,j, BoxTypeEnum.PLAIN);
+            }
+        }
+        this.treasureMap.setGrid(grid);
+    }
+
+
+    @Test
+    void drawMap_with_plain_boxes_only() {
+        String expected = ". . . . .\n. . . . .\n. . . . .\n. . . . .\n. . . . .";
+        assertEquals(expected,outputService.drawMap(this.treasureMap));
+    }
+
+    @Test
+    void drawMap_with_plain_boxes_and_adventurer() {
+        Adventurer adventurer = new Adventurer("Max",0, 0, DirectionEnum.S);
+        this.treasureMap.setAdventurer(adventurer);
+        String expected = "A(Max) . . . .\n. . . . .\n. . . . .\n. . . . .\n. . . . .";
+        assertEquals(expected,outputService.drawMap(this.treasureMap));
+    }
+
+    @Test
+    void drawMap_with_plain_boxes_treasure_box_and_adventurer() {
+        Adventurer adventurer = new Adventurer("Max",0, 0, DirectionEnum.S);
+        this.treasureMap.setAdventurer(adventurer);
+        Box treasureBox = new Box(0,4,3,BoxTypeEnum.TREASURE);
+        Box[][] grid = this.treasureMap.getGrid();
+        grid[0][4] = treasureBox;
+        this.treasureMap.setGrid(grid);
+        this.treasureMap.setAdventurer(adventurer);
+
+        String expected = "A(Max) . . . .\n. . . . .\n. . . . .\n. . . . .\nT(3) . . . .";
+        assertEquals(expected,outputService.drawMap(this.treasureMap));
+    }
+
+    @Test
+    void drawMap_with_plain_boxes_mountain_box_and_adventurer() {
+        Adventurer adventurer = new Adventurer("Max",0, 0, DirectionEnum.S);
+        this.treasureMap.setAdventurer(adventurer);
+        Box mountainBox = new Box(2,2,BoxTypeEnum.MOUNTAIN);
+        Box[][] grid = this.treasureMap.getGrid();
+        grid[2][2] = mountainBox;
+        this.treasureMap.setGrid(grid);
+        this.treasureMap.setAdventurer(adventurer);
+
+        String expected = "A(Max) . . . .\n. . . . .\n. . M . .\n. . . . .\n. . . . .";
+        assertEquals(expected,outputService.drawMap(this.treasureMap));
+    }
+
+    @Test
+    void drawMap_complete_map(){
+        Adventurer adventurer = new Adventurer("Max",0, 0, DirectionEnum.S);
+        this.treasureMap.setAdventurer(adventurer);
+        Box mountainBox = new Box(1,4,BoxTypeEnum.MOUNTAIN);
+        Box treasureBox = new Box(2,4,2,BoxTypeEnum.TREASURE);
+        Box[][] grid = this.treasureMap.getGrid();
+        grid[1][4] = mountainBox;
+        grid[2][4] = treasureBox;
+        this.treasureMap.setGrid(grid);
+        this.treasureMap.setAdventurer(adventurer);
+
+        String expected = "A(Max) . . . .\n. . . . .\n. . . . .\n. . . . .\n. M T(2) . .";
+        assertEquals(expected,outputService.drawMap(this.treasureMap));
+    }
+
+    @Test
+    void writingMap_with_plain_boxes_only() {
+        String expected = "C - 5 - 5";
+        assertEquals(expected,outputService.writingMap(this.treasureMap));
+    }
+    @Test
+    void writingMap_with_plain_boxes_and_adventurer() {
+        Adventurer adventurer = new Adventurer("Max",0, 0, DirectionEnum.S);
+        this.treasureMap.setAdventurer(adventurer);
+
+        String expected = "C - 5 - 5\nA - Max - 0 - 0 - S - 0";
+        assertEquals(expected,outputService.writingMap(this.treasureMap));
+    }
+
+    @Test
+    void writingMap_with_plain_boxes_treasure_box_and_adventurer() {
+        Adventurer adventurer = new Adventurer("Max",0, 0, DirectionEnum.S);
+        this.treasureMap.setAdventurer(adventurer);
+
+        Box treasureBox = new Box(0,4,3,BoxTypeEnum.TREASURE);
+        Box[][] grid = this.treasureMap.getGrid();
+        grid[0][4] = treasureBox;
+        this.treasureMap.setGrid(grid);
+        this.treasureMap.setAdventurer(adventurer);
+
+        String expected = "C - 5 - 5\nT - 0 - 4 - 3\nA - Max - 0 - 0 - S - 0";
+        assertEquals(expected,outputService.writingMap(this.treasureMap));
+    }
+
+    @Test
+    void writingMap_with_plain_boxes_mountain_box_and_adventurer() {
+        Adventurer adventurer = new Adventurer("Max",3, 1, DirectionEnum.E);
+        this.treasureMap.setAdventurer(adventurer);
+
+        Box mountainBox = new Box(2,2,BoxTypeEnum.MOUNTAIN);
+        Box[][] grid = this.treasureMap.getGrid();
+        grid[2][2] = mountainBox;
+        this.treasureMap.setGrid(grid);
+        this.treasureMap.setAdventurer(adventurer);
+
+        String expected = "C - 5 - 5\nM - 2 - 2\nA - Max - 3 - 1 - E - 0";
+        assertEquals(expected,outputService.writingMap(this.treasureMap));
+    }
+
+    @Test
+    void writingMap_complete_map() {
+        Adventurer adventurer = new Adventurer("Lucie",4, 3, DirectionEnum.O);
+        adventurer.setTreasureFound(3);
+        this.treasureMap.setAdventurer(adventurer);
+
+        Box mountainBox = new Box(1,2,BoxTypeEnum.MOUNTAIN);
+        Box treasureBox = new Box(2,4,1,BoxTypeEnum.TREASURE);
+        Box[][] grid = this.treasureMap.getGrid();
+        grid[1][2] = mountainBox;
+        grid[2][4] = treasureBox;
+        this.treasureMap.setGrid(grid);
+        this.treasureMap.setAdventurer(adventurer);
+
+        String expected = "C - 5 - 5\nM - 1 - 2\nT - 2 - 4 - 1\nA - Lucie - 4 - 3 - O - 3";
+        assertEquals(expected,outputService.writingMap(this.treasureMap));
+    }
 }
